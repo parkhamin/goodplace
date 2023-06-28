84f4f8b2d49cf7d3ab36c9f8123d90b0a2724c1e
박하민 <parkhamint@naver.com>
2023-06-28 16:57:49 +0900

    회원가입(2)

--- good_place/src/main/java/com/korea/goodplace/SignupController.java
+++ good_place/src/main/java/com/korea/goodplace/SignupController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import dao.SignupDAO;
+import vo.SignupVO;
 
 @Controller
 public class SignupController {
@@ -15,6 +16,35 @@ public class SignupController {
 	public SignupController(SignupDAO signup_dao) {
 		this.signup_dao = signup_dao;
 	}
+	
+	//회원가입 폼으로 이동
+		@RequestMapping("/signup_insert_form.do")
+		public String moveInsertForm() {
+			return PATH + "signup_insert_form.jsp";
+		}
+		
+		//회원가입
+		@RequestMapping("/insert.do")
+		public String insertUser(SignupVO vo) {
+			
+			int res = signup_dao.insertUser(vo);
+			return "redirect:list.do";
+		}
+		
+		//아이디 중복체크를 위한 메서드
+		@RequestMapping("/check_id.do")
+		@ResponseBody //Ajax로 요청받은 url매핑은 작업완료후 콜백 메서드로 돌아가기 위해 어노테이션 사용
+		public String check_id( String id ) {
+			SignupVO vo = signup_dao.checkId( id );
+			
+			String result = "no";
+			if( vo != null ) {
+				result = "yes";//아이디가 중복될 경우
+			}
+			
+			return result;//result값을 가지고 콜백 메서드로 복귀
+		}
+		
 	
 }
 
